dcf00fd05c3bcbaaf556d14051c0f8219124fa44
김병웅 <quddnddl35@naver.com>
2024-09-29 00:22:09 +0900

    Feat : Cors 설정

---
+++ src/main/java/goormthon/WaterLujah/config/WebCorsConfig.java
@@ -0,0 +1,17 @@
+package goormthon.WaterLujah.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000") // 클라이언트 도메인
+            .allowedMethods("POST")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
