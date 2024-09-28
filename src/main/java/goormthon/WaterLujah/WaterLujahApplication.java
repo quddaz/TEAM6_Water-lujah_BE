dd062580c5ab844ae47597958807c402ffba5e64
김병웅 <quddnddl35@naver.com>
2024-09-28 18:27:26 +0900

    init

---
+++ src/main/java/goormthon/WaterLujah/WaterLujahApplication.java
@@ -0,0 +1,13 @@
+package goormthon.WaterLujah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterLujahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterLujahApplication.class, args);
+	}
+
+}
